9ef6c25556524f61d060a12e1a16302e6d71979f
muskin12345 <syedmuskinmuneera@gmail.com>
2026-04-06 21:02:47 +0530

    Add function to create and remove tasks

---
+++ script.js
@@ -0,0 +1,16 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    if (task === "") return;
+
+    let li = document.createElement("li");
+    li.textContent = task;
+
+    li.onclick = function () {
+        this.remove();
+    };
+
+    document.getElementById("taskList").appendChild(li);
+    input.value = "";
+}
